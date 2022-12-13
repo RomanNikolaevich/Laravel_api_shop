e586483cc13a4b8f70bccbf3bf9b69af6c33e9be
RomanNikolaevich <roman.nikolaevich@gmail.com>
2022-12-13 12:38:30 +0200

    Initial commit

---
+++ app/Http/Requests/Categories/CategoryCreateRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests\Categories;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CategoryCreateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            //
+        ];
+    }
+}
