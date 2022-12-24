c6a537c802c18c12d6014c9e0c2d96bfebb7bf64
RomanNikolaevich <roman.nikolaevich@gmail.com>
2022-12-24 04:39:49 +0200

    currency (controller, service)

---
+++ app/Http/Controllers/Api/V1/CurrencyController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Services\Admin\CurrencyService;
+use GuzzleHttp\Exception\GuzzleException;
+
+class CurrencyController extends Controller
+{
+    /**
+     * @return void
+     * @throws GuzzleException
+     * @throws \JsonException
+     */
+    public static function updateRates():void
+    {
+        $service = new CurrencyService();
+        $service->updateCurrencies(\Carbon\Carbon::tomorrow());
+    }
+
+    /**
+     * @return void
+     * @throws \Exception
+     */
+    public static function readRate():void
+    {
+        $service = new CurrencyService();
+        $service->getCurrencyRateFromDB(\Carbon\Carbon::now(), 'EUR');
+    }
+}
