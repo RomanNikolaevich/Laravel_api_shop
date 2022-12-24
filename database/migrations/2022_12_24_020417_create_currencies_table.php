82ea799c37fc1e011c2c2493995677bce314a3b5
RomanNikolaevich <roman.nikolaevich@gmail.com>
2022-12-24 04:14:44 +0200

    currency (migration)

---
+++ database/migrations/2022_12_24_020417_create_currencies_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('currencies', function (Blueprint $table) {
+            $table->id();
+            $table->string('code'); //currency code
+            $table->integer('rate')->unsigned(); //float converted to integer
+            $table->date('enabled_at'); //the start date of the exchange
+            $table->timestamps();
+
+            $table->index(['code', 'enabled_at']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('currencies');
+    }
+};
